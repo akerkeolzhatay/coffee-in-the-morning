9cc351c08afd988fac2ecb3d4d25258ba284c0a3
akerkeolzhatay <153883779+akerkeolzhatay@users.noreply.github.com>
2025-02-20 23:26:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./config/config.env" });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
